34beabdd7834557270f080a65a3d1a0397fd0ee9
Paulo Jorge <paulojorge@tjro.jus.br>
2019-10-04 14:02:04 -0400

    Adicionando o User

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,12 @@
 const express = required('express');
+const mongoose = required('mongoose');
 const routes = required('./routes');
 
 const app = express();
+mongoose.connect('mongodb+srv://omnistack:stack123456@omnistack-sqguu.mongodb.net/semana09?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.use(express.json());
 app.use(routes);
